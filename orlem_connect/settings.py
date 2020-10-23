cf50d3938fed965ea93bec26eea219e41ebfe681
keane3pereira <keane3pereira@gmail.com>
2020-10-23 14:53:49 +0530

    test

--- orlem_connect/settings.py
+++ orlem_connect/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = ':)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
