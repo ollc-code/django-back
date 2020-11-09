f017d5ffabe79e7c1dc25fd5a7b7dcf35673e04b
keane3pereira <keane3pereira@gmail.com>
2020-11-09 09:46:35 +0530

    announcments post, del

--- announcement/rest/views.py
+++ announcement/rest/views.py
@@ -1,38 +1,13 @@
+from announcement.helpers import set_announcements, cache_todays_announcements, delete_announcement
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from announcement.models import Announcement
 import json
 
 # Create your views here.
 
 ''' Today's announcements cached '''
 TODAY_ANNOUNCEMENTS = False
-
-
-def set_announcements(announcements):
-    Announcement.objects.all().delete()
-    try:
-        for a in announcements:
-            print(a)
-            A = Announcement(text = a)
-            A.save()
-    except Exception as e:
-        print(e)
-        return False
-    return True
-
-
-def cache_todays_announcements():
-    announcements = []
-
-    A = Announcement.objects.all()
-    for a in A:
-        announcements.append(a.text)
-    
-    print(announcements)
-
-    return announcements
 
 
 ### gets or sets today's announcements
@@ -53,6 +28,18 @@ class TodayAnnouncements(APIView):
             announcements = request.data["announcements"]
 
             result = set_announcements(announcements)
+            #print(result)
+            TODAY_ANNOUNCEMENTS = cache_todays_announcements()
+            return Response(result)
+        return Response(False)
+
+    def delete(self, request):
+        global TODAY_ANNOUNCEMENTS
+
+        if request.user.is_staff and request.user.is_authenticated:
+            id = request.data["id"]
+
+            result = delete_announcement(id)
             #print(result)
             TODAY_ANNOUNCEMENTS = cache_todays_announcements()
             return Response(result)
